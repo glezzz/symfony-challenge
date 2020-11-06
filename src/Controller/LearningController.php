f89ca0be7523a0cc751e7a2e1ad58ee545604cf6
Alejandro <alexglezk@gmail.com>
2020-11-06 17:40:37 +0100

    added change and show my name

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -1,7 +1,8 @@
 <?php
-declare(strict_types=1);
 
 namespace App\Controller;
+
+
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,17 +13,31 @@ class LearningController extends AbstractController
     /**
      * @Route("/learning", name="learning")
      */
+    public function index(): Response
+    {
+        return $this->render('learning/index.html.twig', [
+            'controller_name' => 'LearningController',
+        ]);
+    }
+
     public function aboutMe(): Response
     {
-        return $this->render('learning/about-me.html.twig', [
-            'name' => 'Alejandro',
+        return $this->render('aboutMe.html.twig', [
+            'name' => 'Alejandro'
         ]);
     }
 
     public function showMyName(): Response
     {
-        
+        return $this->render('showMyName.html.twig', [
+            'name' => 'Unknown'
+        ]);
     }
 
-
+    public function changeMyName(): Response
+    {
+        return $this->render('changeMyName.html.twig', [
+            'name' => $_POST['new-name']
+        ]);
+    }
 }
